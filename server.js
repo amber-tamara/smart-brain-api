d6430eae51efdb95fb88e79176a3ee432b51cb10
amber-tamara <ambertjansen@gmail.com>
2020-04-07 14:37:59 +0100

    got a bug with JSON

--- server.js
+++ server.js
@@ -1,28 +1,37 @@
 const express = require("express");
-
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
   users: [
     {
       id: "123",
       name: "Amber",
-      email: "amber@gmail.com",
       password: "cookies",
+      email: "amber@gmail.com",
       entries: 0,
       joined: new Date(),
     },
     {
       id: "124",
       name: "Annette",
+      password: "art",
       email: "annette@gmail.com",
-      password: "bananas",
       entries: 0,
       joined: new Date(),
+    },
+  ],
+  login: [
+    {
+      id: "987",
+      hash: "",
+      email: "amber@gmail.com",
     },
   ],
 };
@@ -32,6 +41,21 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
+  // Load hash from your password DB.
+  bcrypt.compare(
+    "banana",
+    "$2a$10$q/DmvdsS9o.SRoxHUOR2KeyUIeg7V/9y.19PXezrnVqskwzrJ1q5e",
+    function (err, res) {
+      console.log("first guess:", res);
+    }
+  );
+  bcrypt.compare(
+    "veggies",
+    "$2a$10$q/DmvdsS9o.SRoxHUOR2KeyUIeg7V/9y.19PXezrnVqskwzrJ1q5e",
+    function (err, res) {
+      console.log("Second guess:", res);
+    }
+  );
   if (
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
@@ -44,6 +68,10 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
+  bcrypt.hash(password, null, null, function (err, hash) {
+    console.log(hash);
+  });
+
   database.users.push({
     id: "125",
     name: name,
@@ -53,6 +81,35 @@ app.post("/register", (req, res) => {
     joined: new Date(),
   });
   res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
+});
+
+app.post("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
+  }
 });
 
 app.listen(3000, () => {
